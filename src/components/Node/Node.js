359a92bb518ec8cb00132853c7d4032476b23c39
Dima Vyshniakov <morewings@gmail.com>
2020-10-11 11:52:32 +0200

    add description

--- src/components/Node/Node.js
+++ src/components/Node/Node.js
@@ -64,11 +64,25 @@ const Node = ({
         </header>
         {isActive && (
           <main>
-            <div>children: {children.length}</div>
-            <div>completion: {completion}%</div>
-            <div>color: {color}</div>
+            <div className={classes.stats}>
+              <h3>Stats</h3>
+              <div className={classes.chart}>{completion}</div>
+              <div>
+                <div className={classes.tier}>
+                  <h4>Tier</h4>
+                  <div className={classes.amount}>4th</div>
+                </div>
+                <div className={classes.ancestors}>
+                  <h4>Ancestors</h4>
+                  <div className={classes.amount}>{children.length}</div>
+                </div>
+              </div>
+            </div>
             {description && (
-              <div className={classes.description}>{description}</div>
+              <div className={classes.description}>
+                <h3>Description</h3>
+                <div className={classes.text}>{description}</div>
+              </div>
             )}
             <Button onClick={handleSelect}>Show children</Button>
             <Button onClick={handleModalShow}>Edit node</Button>
