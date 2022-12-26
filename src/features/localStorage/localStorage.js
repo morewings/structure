57618b953286b34d1eec0c567515e0857bfd7157
Dima Vyshniakov <morewings@gmail.com>
2022-12-26 18:16:24 +0100

    fix

--- src/features/localStorage/localStorage.js
+++ src/features/localStorage/localStorage.js
@@ -2,7 +2,8 @@ import config from '@/config';
 
 export const loadState = () => {
   try {
-    const serializedState = localStorage?.getItem(config.localstorageKey);
+    const serializedState =
+      typeof window !== 'undefined' ? window.localStorage.getItem(config.localstorageKey) : null;
     if (serializedState === null) {
       return undefined;
     }
@@ -16,7 +17,7 @@ export const loadState = () => {
 export const saveState = state => {
   try {
     const serializedState = JSON.stringify(state);
-    localStorage?.setItem(config.localstorageKey, serializedState);
+    typeof window !== 'undefined' && window.localStorage.setItem(config.localstorageKey, serializedState);
   } catch {
     console.error(`Can't save to localStorage`); // TODO: add toast
   }
