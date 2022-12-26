b0d72c7fc4ee4a15245d7a66b8741dd07348d273
Dima Vyshniakov <morewings@gmail.com>
2022-12-26 17:51:19 +0100

    fix

--- src/components/HTMLHead/HTMLHead.js
+++ src/components/HTMLHead/HTMLHead.js
@@ -5,7 +5,10 @@ export const HTMLHead = ({children, title}) => {
   return (
     <Head>
       <meta name="description" content="Tree graph view app" />
-      <link type="image/png" rel="icon" href="/favicon.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
       <title>{`Structure | ${title}`}</title>
       {children}
     </Head>
