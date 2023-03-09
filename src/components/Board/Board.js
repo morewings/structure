d7adeac94b5835044be07d81b7bd0b429dc2d1dd
Dima Vyshniakov <morewings@gmail.com>
2023-03-09 20:01:45 +0100

    fix localState tooltip

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import useBoardContent from '@/features/structure/useBoardContent';
 import {useSaveState, useHydrateState} from '@/features/localStorage';
@@ -9,13 +9,15 @@ import Column from '@/components/Column';
 export const Board = () => {
   useSaveState();
   const {hydrateState} = useHydrateState();
+  const [hasLocalState, setHasLocalState] = useState();
   const showToast = useInfoToast();
   useEffect(() => {
     const hasLocalState = hydrateState();
-    return () => {
-      hasLocalState && showToast({text: 'Loaded data from local storage'});
-    };
-  }, [hydrateState, showToast]);
+    setHasLocalState(hasLocalState);
+  }, [hydrateState]);
+  useEffect(() => {
+    hasLocalState && showToast({text: 'Loaded data from local storage'});
+  }, [hasLocalState, showToast]);
   const {childrenId, siblingsId, parentId} = useBoardContent();
   return (
     <div>
